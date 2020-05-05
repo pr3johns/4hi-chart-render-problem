c9510f5f8a5a3debd7cad743a714434634141595
Agustin Lebron <alebron@agustins-mbp.lan>
2020-05-05 09:51:03 -0700

    fixed the header

--- src/components/heading.jsx
+++ src/components/heading.jsx
@@ -1,20 +1,40 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import logo from './logo_copy.png';
+import useWindowDimensions from '../windowDimensions';
 
 export default function Heading() {
+  const { height, width } = useWindowDimensions();
+
+  console.log('Width');
+  console.log(width);
   return (
-    <Navbar bg="dark" variant="dark">
-      <Navbar.Brand href="#home">
-        <img
-          src={logo}
-          width="50"
-          height="50"
-          className="d-inline-block align-top"
-          alt=""
-        />
-        &nbsp; 2020 Annual Tech Leader Survey
-      </Navbar.Brand>
-    </Navbar>
-  );
+    (width < 500) ?
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">
+          <img
+            src={logo}
+            width="100"
+            height="100"
+            className="d-inline-block align-top"
+            alt=""
+          />
+          <br></br>2020 Annual Tech<br></br>Leader Survey
+        </Navbar.Brand>
+      </Navbar>
+    :
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">
+          <img
+            src={logo}
+            width="60"
+            height="60"
+            className="d-inline-block align-top"
+            alt=""
+          />
+          &nbsp; 2020 Annual Tech Leader Survey
+        </Navbar.Brand>
+      </Navbar>
+
+);
 }
